ce3a029c8b8349f9c5847dadb56501ec1c404a31
sebastian <plxcxs@protonmail.com>
2026-08-07 18:31:57 +0200

    resolved merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -8,35 +8,13 @@ function App() {
             <h1>Theme Creator</h1>
             {initialColors.map((color) => {
                 return (
-                    <Card
-                        key={color.id}
-                        hex={color.hex}
-                        role={color.role}
-                        contrastText={color.contrastText}
-                    />
+                    <>
+                        <Color key={color.id} color={color} hex={color.hex} />
+                    </>
                 );
             })}
-
-            <Card />
         </>
     );
 }
 
 export default App;
-export function Card({ hex, role, contrastText }) {
-    return (
-        <article>
-            <div
-                id="color_card"
-                style={{
-                    backgroundColor: hex,
-                    color: contrastText,
-                    width: "200px",
-                }}
-            >
-                {" "}
-                {hex} {role}
-            </div>
-        </article>
-    );
-}
--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -1,5 +1,22 @@
 import "./Color.css";
 
 export default function Color({ color }) {
-  return null;
+    return (
+        <article>
+            <div
+                className="color-card"
+                style={{
+                    backgroundColor: color.hex,
+                    color: color.contrastText,
+                    width: "200px",
+                }}
+            >
+                <span>
+                    <div className="color-card-headline"> hex: {color.hex}</div>
+                    <div>role: {color.role}</div>{" "}
+                    <div>contrast: {color.contrastText}</div>
+                </span>{" "}
+            </div>
+        </article>
+    );
 }
