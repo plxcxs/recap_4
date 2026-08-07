dc4c2a54162115328d729706d67e0da5833d5f7c
sebastian <plxcxs@protonmail.com>
2026-08-07 16:39:08 +0200

    added first Criteria

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,45 @@ import Color from "./Components/Color/Color";
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      <h1>Theme Creator</h1>
-      // display colors here
-    </>
-  );
+    return (
+        <>
+            <h1>Theme Creator</h1>
+            {initialColors.map((color) => {
+                return (
+                    <Card
+                        key={color.id}
+                        hex={color.hex}
+                        role={color.role}
+                        contrastText={color.contrastText}
+                    />
+                );
+            })}
+
+            <Card />
+        </>
+    );
 }
 
+/* const role = "role";
+const hex = "hex";
+const color = "red"; */
+console.log("Hello friend !");
 export default App;
+export function Card({ hex, role, contrastText }) {
+    return (
+        <article>
+            <a href="https://youtube.com"></a>
+            <div
+                id="color_card"
+                style={{
+                    backgroundColor: hex,
+                    color: contrastText,
+                    width: "200px",
+                }}
+            >
+                {" "}
+                {hex} {role}
+            </div>
+        </article>
+    );
+}
