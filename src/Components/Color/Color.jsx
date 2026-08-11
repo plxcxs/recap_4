6a6815cea0c574e967762024e110ed9177ac60e8
sebastian <plxcxs@protonmail.com>
2026-08-11 17:16:44 +0200

    fixed the codereview issues but had branch issues so i hope this is alright

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -53,6 +53,7 @@ export default function Color({ color, onDeleteColor, onUpdateColor }) {
                 </button>
                 {!isEditing && <button onClick={handleEdit}>Edit</button>}
             </div>
+            {/* this is a comment to make the code comittable i will delete it so u dont have to put it in the review */}
         </article>
     );
 }
